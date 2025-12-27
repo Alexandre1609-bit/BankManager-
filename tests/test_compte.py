7d669984a1228726f64eb1bb8637a43ebf5b3050
Alexandre <alexandre.regnier59@live.fr>
2025-12-27 17:37:50 +0100

    test: update tests with db fixture

--- tests/test_compte.py
+++ tests/test_compte.py
@@ -1,43 +1,64 @@
-from src.compte import CompteBancaire
 import pytest
+import sqlite3
+import os
+import sys
 
-@pytest.fixture
-def compte_paul():
-    """Crée un compte par défaut pour les tests"""
-    return CompteBancaire("Paul")
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 
-def test_deposer_argent(compte_paul):
+from compte import CompteBancaire
 
-    compte_paul.deposer(100)
 
-    assert compte_paul.solde == 100
-
-def test_retrait_impossible(compte_paul):
-    compte_paul.deposer(50)
-
-    #On parie que ce qui suit plantera avec une ValueError
-    with pytest.raises(ValueError):
-        compte_paul.retirer(200)
-
-def test_retrait_valide(compte_paul):
-    """Vérifie qu'un retrait normal fonctionne"""
+@pytest.fixture(autouse=True)
+def setup_database():
+    """
+    Cette fonction s'exécute automatiquement AVANT et APRÈS chaque test.
+    """
     
-    compte_paul.deposer(100)
+    with sqlite3.connect("bank.db") as con:
+        cur = con.cursor()
+        cur.execute("DROP TABLE IF EXISTS comptes") 
+        cur.execute("""
+            CREATE TABLE comptes (
+                nom TEXT PRIMARY KEY,
+                solde REAL NOT NULL
+            );
+        """)
+        
+        cur.execute("INSERT INTO comptes (nom, solde) VALUES (?, ?)", ("TestUser", 100.0))
+        con.commit()
 
-    compte_paul.retirer(40)
+    yield 
 
-    assert compte_paul.solde == 60.0
+    if os.path.exists("bank.db"):
+        os.remove("bank.db")
 
-def test_virement_valide(compte_paul):
-  
-    receveur = CompteBancaire("Pierre")
-    compte_paul.deposer(100)
+def test_creation_compte_existant():
+    """Vérifie qu'on récupère bien le solde de la BDD"""
+    compte = CompteBancaire("TestUser") 
+    assert compte.solde == 100.0
 
-    compte_paul.virement(receveur, 30)
+def test_creation_nouveau_compte():
+    """Vérifie la création d'un nouveau client"""
+    compte = CompteBancaire("Nouveau")
+    assert compte.solde == 0.0
+    
+    with sqlite3.connect("bank.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT solde FROM comptes WHERE nom = 'Nouveau'")
+        assert cur.fetchone() is not None
 
-    assert compte_paul.solde == 70.0
-    assert receveur.solde == 30.0
+def test_depot():
+    compte = CompteBancaire("TestUser")
+    compte.deposer(50.0)
+    
+    assert compte.solde == 150.0
+    
+    with sqlite3.connect("bank.db") as con:
+        cur = con.cursor()
+        res = cur.execute("SELECT solde FROM comptes WHERE nom = 'TestUser'").fetchone()
+        assert res[0] == 150.0
 
-def test_depot_neg(compte_paul):
+def test_retrait_erreur():
+    compte = CompteBancaire("TestUser")
     with pytest.raises(ValueError):
-        compte_paul.deposer(-20)
+        compte.retirer(200.0)
